c9dd785c094bb58c965befa1b1cfe8b270ea5f0b
NUNES Pierre <o22103139@info27-21.iut45.univ-orleans.fr>
2023-03-31 14:42:41 +0200

    [ADD] ajout du css

--- sfmusic_nunes_foltier/src/Controller/indexController.php
+++ sfmusic_nunes_foltier/src/Controller/indexController.php
@@ -3,13 +3,12 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-class indexController extends AbstractController
-{
+class indexController extends AbstractController{
 
 /**
 * @Route("/")
 */
 public function open(){
     return $this->render('accueil.html.twig');
-}
+    }
 }
